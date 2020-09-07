72cbbef8e43774356a8b858cd40bd87a26a4ae87
Luélly Cabral <luellycabral21@gmail.com>
2020-09-06 22:00:50 -0300

    Adicionar a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace Etec1AB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etec Adolpho Berezin");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 **Iniciando Programação**:
 
 Desenvolva um programa que exiba o nome da escola: `Etec Adolpho Berezin`
+
+**como executar** 
+
+Ao acessar a pasta do projeto só digitar:
+
+```
+dotnet run
+```
